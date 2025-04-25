ae4871a36e13b99553ed06b768e9736ce2fe7b53
Baptiste Dulauroy <dulauroybaptiste@gmail.com>
2025-04-25 10:42:11 +0200

    Radio Dialogue Box
    Dans une zone, si le joueur rentre, une boite de dialogue s'active et à la fin la zone est détruite

---
+++ Assets/Script/RadioManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadioManager : MonoBehaviour
+{
+
+    public static RadioManager instance; // Instance unique pour pouvoir y accéder facilement (pattern Singleton)
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogText;
+    private Queue<string> sentences;//on va mettre les phrases du dialogue dans une liste, Queue va avoir plusieurs élèments les un derriere les autres
+    public Image DialogueUI;
+    private Shoot shootScript;
+    public RadioTrigger radioTrigger;
+
+
+    public System.Action onDialogueEnd; // on permet aux autres scripts d'écouter la fin du dialogue
+
+    void Awake()
+    {
+        if(instance != null) // Vérifie si une autre instance existe déjà
+        {
+            Debug.LogWarning("Il y a plus d'une instance de dialoguemanager dans la scène");
+            return;
+        }
+        instance = this; // Assigne l'instance actuelle comme référence globale
+
+        sentences = new Queue<string>(); //on va initialiser notre queue
+        DialogueUI.gameObject.SetActive(false); // Cache l’interface du dialogue au début
+
+        shootScript = FindObjectOfType<Shoot>();// récupère le script Shoot attaché au joueur
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        if (shootScript != null) //on désactive l'arme pour éviter de tirer quand un dialogue se lance
+        {
+            shootScript.StatueWeaponOff(); // Désactive l’arme du joueur pendant le dialogue
+        }
+
+        DialogueUI.gameObject.SetActive(true);
+        nameText.text = dialogue.name;
+        sentences.Clear(); //on vide dans le doute la liste d'attente, pour éviter que le dialogue précédent y soit encore
+
+        foreach (string sentence in dialogue.sentences) //boucle pour mettre chacune des phrases dans la file d'attente
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence() //on le met en public pour le buttin continue
+    {
+        if(sentences.Count == 0) //si la file est vide, terminer le dialogue
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue(); //on récupére le prochain élément de la file d'attente
+        StopAllCoroutines(); //dans le doute on stoppe d'abord toute les coroutines pour éviter une supperposition
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogText.text = "";
+        foreach (char letter in sentence.ToCharArray())//on découpe notre phrase en array
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(0.02f); //on donne une légère latence de 0.02 seconde
+        }
+    }
+
+    void EndDialogue()
+    {
+        DialogueUI.gameObject.SetActive(false); // on cache l’interface du dialogue
+        onDialogueEnd?.Invoke(); // on ignale que le dialogue est terminé
+    }
+
+
+}
---
+++ Assets/Script/RadioTrigger.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+
+public class RadioTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public bool isInRange;
+    private bool hasTriggered = false;
+    private int currentSentenceIndex = 0;
+
+    void Update()
+    {
+        if (isInRange && !hasTriggered)
+        {
+            hasTriggered = true;
+            TriggerDialogue();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+           isInRange = true;
+     }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+      {
+          isInRange = false;
+      }
+    }
+
+    void TriggerDialogue()
+    {
+        RadioManager.instance.onDialogueEnd += OnDialogueEnd; // on s’abonne à la fin du dialogue
+        RadioManager.instance.StartDialogue(dialogue);
+    }
+
+    void OnDialogueEnd()
+    {
+        if(currentSentenceIndex == dialogue.sentences.Length -1 )//si on atteint la derniere phrase du dialogue
+        {
+            RadioManager.instance.onDialogueEnd -= OnDialogueEnd; // on se désabonne proprement
+            StartCoroutine(DestroyAfterDialogue());
+        }
+        else
+        {
+            currentSentenceIndex ++;
+        }
+    }
+
+    private IEnumerator DestroyAfterDialogue()
+    {
+        yield return new WaitForSeconds(1f);// Attendre un moment (1 seconde) pour être sûr que le dernier texte est lu
+        Destroy(gameObject); // Détruire l'objet après le délai
+    }
+}
