f40d0cda3906d40bd3530605d3ac0e17fa7c5c11
Dorian Durand <doriandurand@hotmail.fr>
2025-05-14 16:59:50 +0200

    Fade au début du level + Erreur anima inv patch
    Il y a un fade out au début du level pour une meilleure transition et finit les codes d'erreurs pour l'inv ... fallait juste le remettre dans le canva décoché

--- Assets/Script/PlayerSpawn.cs
+++ Assets/Script/PlayerSpawn.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
 
 public class PlayerSpawn : MonoBehaviour
 {
+
     private void Awake()
     {
+        // Déplacement du joueur au point de spawn
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
             player.transform.position = transform.position;
         }
     }
-
 }
